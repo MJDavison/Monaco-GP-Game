3a0936ac0362a84123d4faa7c9686bec3be7cac0
MJDeveloping <63269417+MJDeveloping@users.noreply.github.com>
2020-12-28 23:25:29 +0000

    Implemented the "Infinite Road".

---
+++ Monaco-GP-Game/Assets/RoadMover.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoadMover : MonoBehaviour
+{
+    [SerializeField] RoadManager roadManager;
+    
+    
+    private void OnEnable() {
+        GameObject gameController = GameObject.FindGameObjectWithTag("GameController");
+        roadManager = gameController.GetComponent<RoadManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.down * Time.deltaTime * roadManager.roadPassingSpeed);
+    }
+}
